9b903ba9c46e42e0c4d80b9ad55f214e62514ebb
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-04-27 20:00:14 -0300

    fix: fixing tests suites

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,6 +3,7 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
     file = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
-    assert "title" in file
-    assert "salary" in file
-    assert "type" in file
+    for job in file:
+        assert 'title' in job
+        assert 'salary' in job
+        assert 'type' in job
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -5,5 +5,5 @@ from src.pre_built.counter import count_ocurrences
 def test_counter():
     assert count_ocurrences("data/jobs.csv", "end") == 5889
     assert count_ocurrences("data/jobs.csv", "END") == 5889
-    with pytest.raises(TypeError):
+    with pytest.raises(AttributeError):
         count_ocurrences("data/jobs.csv", 4)
