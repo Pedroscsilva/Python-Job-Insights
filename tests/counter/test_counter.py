fe9a6f0da9b4172cfb51d6797b302994347ac64a
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-04-27 19:37:46 -0300

    feat: adding tests for the word counting func

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("tests/mocks/jobs_with_types.csv", "end") == 3
+    assert count_ocurrences("tests/mocks/jobs_with_types.csv", "END") == 3
+    with pytest.raises(TypeError):
+        count_ocurrences("tests/mocks/jobs_with_types.csv", 4)
