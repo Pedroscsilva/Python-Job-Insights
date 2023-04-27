a1e358fd1c0a59d3df50974f294ef40d06b93ac2
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-04-27 20:34:07 -0300

    feat: adding tests for the sort func

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,31 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+unordered_jobs = [
+    {"min_salary": 1000, "max_salary": 4200, "date_posted": "2020-05-08"},
+    {"min_salary": 1500, "max_salary": 4000, "date_posted": "2020-05-11"},
+    {"min_salary": 700, "max_salary": 2200, "date_posted": "2020-05-15"}
+]
+
+min_ordered = [
+    {"min_salary": 700, "max_salary": 4200, "date_posted": "2020-05-15"},
+    {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-05-08"},
+    {"min_salary": 1500, "max_salary": 4000, "date_posted": "2020-05-11"}
+]
+
+max_ordered = [
+    {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-05-08"},
+    {"min_salary": 1500, "max_salary": 4000, "date_posted": "2020-05-11"},
+    {"min_salary": 700, "max_salary": 4200, "date_posted": "2020-05-15"}
+]
+
+date_ordered = [
+    {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-05-08"},
+    {"min_salary": 1500, "max_salary": 4000, "date_posted": "2020-05-11"},
+    {"min_salary": 700, "max_salary": 4200, "date_posted": "2020-05-15"}
+]
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(unordered_jobs, "min_salary") == min_ordered
+    assert sort_by(unordered_jobs, "max_ordered") == max_ordered
+    assert sort_by(unordered_jobs, "date_ordered") == date_ordered
