d7149fb5c2e89d262dd1bed40fb3e8d8137797d3
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-04-27 21:08:07 -0300

    fix: sort_by modifies the recieved list apparently

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,31 +1,25 @@
+import pytest
 from src.pre_built.sorting import sort_by
 
-unordered_jobs = [
-    {"min_salary": 1000, "max_salary": 4200, "date_posted": "2020-05-08"},
-    {"min_salary": 1500, "max_salary": 4000, "date_posted": "2020-05-11"},
-    {"min_salary": 700, "max_salary": 2200, "date_posted": "2020-05-15"}
-]
 
-min_ordered = [
-    {"date_posted": "2020-05-15", "max_salary": 4200, "min_salary": 700},
-    {"date_posted": "2020-05-08", "max_salary": 2000, "min_salary": 1000},
-    {"date_posted": "2020-05-11", "max_salary": 4000, "min_salary": 1500}
-]
-
-max_ordered = [
-    {"date_posted": "2020-05-15", "max_salary": 4200, "min_salary": 700},
-    {"date_posted": "2020-05-11", "max_salary": 4000, "min_salary": 1500},
-    {"date_posted": "2020-05-08", "max_salary": 2000, "min_salary": 1000}
-]
-
-date_ordered = [
-    {"date_posted": "2020-05-15", "max_salary": 4200, "min_salary": 700},
-    {"date_posted": "2020-05-11", "max_salary": 4000, "min_salary": 1500},
-    {"date_posted": "2020-05-08", "max_salary": 2000, "min_salary": 1000}
-]
+@pytest.fixture
+def unordered_jobs():
+    return [
+        {"date_posted": "2020-05-08", "max_salary": 2000, "min_salary": 1000},
+        {"date_posted": "2020-05-11", "max_salary": 4000, "min_salary": 1500},
+        {"date_posted": "2020-05-15", "max_salary": 4200, "min_salary": 700}
+    ]
 
 
-def test_sort_by_criteria():
-    assert sort_by(unordered_jobs, "min_salary") == min_ordered
-    assert sort_by(unordered_jobs, "max_ordered") == max_ordered
-    assert sort_by(unordered_jobs, "date_ordered") == date_ordered
+def test_sort_by_criteria(unordered_jobs):
+    copy_of_jobs = unordered_jobs.copy()
+    sort_by(copy_of_jobs, "min_salary")
+    assert copy_of_jobs[0] == unordered_jobs[1]
+
+    copy_of_jobs = unordered_jobs.copy()
+    sort_by(copy_of_jobs, "max_salary")
+    assert copy_of_jobs[0] == unordered_jobs[2]
+
+    copy_of_jobs = unordered_jobs.copy()
+    sort_by(copy_of_jobs, "date_posted")
+    assert copy_of_jobs[0] == unordered_jobs[2]
